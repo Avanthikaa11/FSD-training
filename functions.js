f06efd7cb8f9c041396c6ce9673ea6f013171c74
Avanthikaa11 <vagdeviavanthika@gmail.com>
2025-01-08 13:19:10 +0530

    added 8-1-25 files to the repo

---
+++ functions.js
@@ -0,0 +1,14 @@
+//! without parameters and arguments
+
+// function demo() {   //? function declaration
+//   console.log("HIIIIIIIIIIIIII iam functionnnn");
+// }
+// demo(); //? function calling
+
+//! with parameters and arguments
+
+function addition(a, b) {
+  console.log(a + b);
+}
+addition(5, 7); //? function calling with arguments
+addition(10, 11); //? functions are reusable
